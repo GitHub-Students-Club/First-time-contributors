2d23d83356b7ae3e9be60d096e24ec78ad00342f
pradeep-bhandari <pradeep.bhandari.2703@gmail.com>
2021-10-09 14:24:39 +0630

    Added Pradeep Bhandari to contributors list

--- Java Enthusiasts/Demo.java
+++ Java Enthusiasts/Demo.java
@@ -6,6 +6,6 @@ The file name should be yourname.java
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello my name is John. ");
+        System.out.println("Hello my name is Pradeep");
     }
 }
